f154c7657f802825e64294640a8e56cad8af3adf
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2020-03-22 23:36:32 -0300

    se agrego atributo componente aplicatico en articulo de tipoia

--- softwareIA/aplicaciones/baseSistema/models.py
+++ softwareIA/aplicaciones/baseSistema/models.py
@@ -8,6 +8,7 @@ class TipoIA(models.Model):
     title = models.CharField(max_length = 50)
     src = models.URLField(max_length=300, default = "https://source.unsplash.com/UUjxTEET0c0/1200x560")
     description = models.TextField()
+    componentApp = models.CharField(max_length = 70, null = True, blank = True)
     #creationDate =  models.DateField('creation date', auto_now=False,auto_now_add=True)
     #tags, 
     # secciones(image or text or video)
